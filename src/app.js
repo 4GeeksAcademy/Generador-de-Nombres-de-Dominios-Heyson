d851cad0b597ab4d09a14cc0df02290de8b018c6
Heyson Betancourt <146369401+heysonbr@users.noreply.github.com>
2023-12-22 19:02:53 +0000

    acabado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateDomains(pronouns, adjectives, nouns, extensions) {
+    const generatedDomains = [];
+
+    for (let pronoun of pronouns) {
+      for (let adjective of adjectives) {
+        for (let noun of nouns) {
+          for (let extension of extensions) {
+            let domain = pronoun + adjective + noun + extension;
+            generatedDomains.push(domain);
+
+            if (extension === ".es") {
+              let hackDomain = pronoun + adjective + noun + "s" + extension;
+              generatedDomains.push(hackDomain);
+            }
+          }
+        }
+      }
+    }
+
+    return generatedDomains;
+  }
+
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io", ".es"];
+
+  let generatedDomains = generateDomains(
+    pronouns,
+    adjectives,
+    nouns,
+    extensions
+  );
+  let generatorDiv = document.getElementById("generator");
+  generatorDiv.innerHTML = generatedDomains.join("<br>");
+  console.log(generatedDomains);
 };
